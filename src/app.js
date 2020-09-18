749d7d626cd2237778ac2465d78ddf3ee0b56da5
imbenrabi <imbenrabi@gmail.com>
2020-09-18 14:48:57 +0300

    struct setup

--- src/app.js
+++ src/app.js
@@ -7,8 +7,14 @@ const usersRouter = require('./routers/users');
 
 const distPath = path.join(__dirname, '../dist');
 const nodeModulesPath = path.join(__dirname, '../node_modules');
+const viewsPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials')
 
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(distPath));
 app.use(express.static(nodeModulesPath))
